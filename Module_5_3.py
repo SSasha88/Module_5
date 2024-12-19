6426fddfab9e02c4c262789770cf32b09c68c4c0
SSasha88 <d1rus1853@gmail.com>
2024-12-19 16:44:54 +0300

    + Решаю ... Задача "Нужно больше этажей":

--- Module_5_3.py
+++ Module_5_3.py
@@ -3,7 +3,7 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-    def __str__(self):# - должен возвращать строку: "Название: <название>, кол-во этажей: <этажи>"
+    def __str__(self):
         return f'{self.name}, кол-во этажей: {self.number_of_floors}'
     def __len__(self):
         return self.number_of_floors
@@ -14,18 +14,33 @@ class House:
                 print( f'{self.name}, этаж:', floor)
         else:
             print(f'В {self.name} Такого этажа не существует')
+    #Метод для сравнения количества этажей
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return  self.number_of_floors == other. number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == number_of_floors
 
+    # Метод для проверки меньше чем (<)
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
+    # Метод для проверки меньше либо равно (<=)
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other. number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
 
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
-print()
-# __str__
+#h2 = [2, 3]
 print(h1)
 print(h2)
-# __len__
-print(len(h1))
-print(len(h2))
-#h1.go_to(5)
-#h2.go_to(10)
+print('"h1=h2"', h1 == h2) # __eq__
+print('"h1<h2"', h1 < h2) # __lt__
+print('"h1<=h2"', h1 <= h2) # __le__
